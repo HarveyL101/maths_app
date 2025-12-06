d05c78233031e44398f16c9227f5ee3d2db1dc90
Harvey Lopez <up2118183@myport.ac.uk>
2025-12-06 23:31:38 +0000

    Added: Files to help flesh out the homepage, all a WIP

---
+++ client/src/components/SideBar.jsx
@@ -0,0 +1,24 @@
+const SideBar = () => {
+  return (
+    // Sidebar (Might be used idk)
+      <div className="max-w-1/5 h-full">
+        <div className="border border-solid-black">
+          <h3>Sidebar</h3>
+        </div>
+        <ul className="border border-solid-black">
+          <li>Poo bum</li>
+          <li>Poo bum</li>
+          <li>Poo bum</li>
+          <li>Poo bum</li>
+          <li>Poo bum</li>
+          <li>Poo bum</li>
+          <li>Poo bum</li>
+          <li>Poo bum</li>
+          <li>Poo bum</li>
+          <li>Poo bum</li>
+        </ul>
+      </div>
+  );
+};
+
+export default SideBar;
---
+++ client/src/components/TitleBar.jsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+
+const TitleBar = () => {
+  return (
+    <div className="w-full">
+      {/* --- Top Row --- */}
+      <div className="flex items-center justify-between w-full px-4 py-2">
+
+        {/* Left: logo */}
+        <Link to='/home'>
+          <img 
+            className='object-contain w-20 border border-gray-700 rounded-full p-1 shadow-lg hover:shadow-3xl transition-shadow duration-200 cursor-pointer' 
+            src="/logo.png" 
+            alt="App Logo" 
+            title='Press me for the homepage!'
+          />
+        </Link>
+        
+        {/* Center: title */}
+        <h1 className='text-2xl font-bold text-center flex-1'><u>Number Ninjas</u></h1>
+
+        {/* Right: profile picture */}
+        <Link to='/profile'>
+          <img 
+            className='object-contain w-20 border border-gray-700 rounded-full p-1 shadow-lg hover:shadow-3xl transition-shadow duration-200 cursor-pointer'
+            src="/default_pfp.png" 
+            alt="Profile Picture" 
+            title='Press me to see your profile!'
+          />
+        </Link>
+      </div>
+
+        <nav className='w-full border-t pt-0.5'>
+          <ul className='flex justify-center gap-4 text-lg'>
+            <li className='nav-item border border-gray-400 bg-gray-300'><Link to='/home'>Home</Link></li>
+            <li className='nav-item text-blue-500 border solid-blue-500 hover:bg-blue-500 hover:text-white transition'>Lessons</li>
+            <li className='nav-item text-orange-400 border solid-orange-400 hover:bg-orange-400 hover:text-white transition'>Practice/ Learn</li>
+            <li className='nav-item text-green-700 border solid-green-700 hover:bg-green-700 hover:text-white transition'>Tips / Help</li>
+            <li className='nav-item text-purple-500 border solid-purple-500 hover:bg-purple-500 hover:text-white transition'>Profile</li>
+          </ul>
+        </nav>
+      </div>
+  );
+};
+
+export default TitleBar;

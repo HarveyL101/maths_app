f27c24db9ec2c75f6d1cd891bfdeccae62403966
Harvey Lopez <up2118183@myport.ac.uk>
2025-12-06 23:32:03 +0000

    Improved home page functionality, beginning to add the skeleton for content to expand from

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,46 @@
+import { SideBar, TitleBar } from '../utils/index';
+
 const Home = () => {
   return (
     <div>
-      <h1>Home Page!</h1>
+      <TitleBar />
+
+      <div className='pt-4 pl-3 pr-3'>
+        <h2 className='font-bold text-3xl text-gray-700'>Browse...</h2>
+        <div className='grid grid-cols-2 gap-4'>
+          <div className='card'>
+            <h3 className='card-header'>Card Header</h3>
+            <p className='card-content'>Card Content</p>
+            <footer>
+              <p className='card-footer'>Card footer</p>
+            </footer>
+          </div>
+
+          <div className='card'>
+            <h3 className='card-header'>Card Header</h3>
+            <p className='card-content'>Card Content</p>
+            <footer>
+              <p className='card-footer'>Card footer</p>
+            </footer>
+          </div>
+
+          <div className='card'>
+            <h3 className='card-header'>Card Header</h3>
+            <p className='card-content'>Card Content</p>
+            <footer>
+              <p className='card-footer'>Card footer</p>
+            </footer>
+          </div>
+
+          <div className='card'>
+            <h3 className='card-header'>Card Header</h3>
+            <p className='card-content'>Card Content</p>
+            <footer>
+              <p className='card-footer'>Card footer</p>
+            </footer>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

74ac372fd27b89809e897edfd3fad97a7ab31d10
Harvey Lopez <up2118183@myport.ac.uk>
2025-12-06 23:30:31 +0000

    Cleanup of App.jsx, streamlined import statements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,14 @@
-import { useState, useEffect } from 'react';
-import axios from "axios";
 import { Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home';
+import { Login, Register, Home, Profile } from './utils/index';
 
 const App = () => {
-  const [array, setArray] = useState([]);
-
-  const fetchAPI = async () => {
-    try {
-      const response = await axios.get("http://localhost:8080/api");
-
-      setArray(response.data.lessons);
-
-      console.log(response.data.lessons);
-    } catch(error) {
-      console.error("API error:", error.message);
-    }
-  }
-
-  useEffect(() => {
-    fetchAPI();
-  }, []);
-
   return (
     <>
-      <h1>Maths Landing Page</h1>
-
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/home' element={<Home />}/>
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </>
   )

4f76e899b7bc120f9e48245dcf1e89a394c95755
Harvey <UP2118183@myport.ac.uk>
2026-01-26 20:25:26 +0000

    Added tables for lessons and questions, trying to flesh out the skeleton

--- server/schema.sql
+++ server/schema.sql
@@ -1,3 +1,4 @@
+-- USERS TABLE --
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name CHAR(20) NOT NULL,
@@ -6,15 +7,54 @@ CREATE TABLE users (
   created_at CURRENT_TIMESTAMP(2)
 );
 
--- Lessons Table here
-
--- Test Variables
 INSERT INTO users (name, email, password) VALUES
+  ('Admin', 'Admin@gmail.com', 'admin123'),
   ('Ella', 'Ellajoyce50@gmail.com', 'EllasPassword'),
   ('Harvey', 'Harveylopez50@gmail.com', 'HarveysPassword'),
   ('Alex', 'Alexnicholson50@gmail.com', 'AlexsPassword');
 
+-- EDUCATORS TABLE --
+CREATE TABLE educators (
+  id SERIAL PRIMARY KEY,
+  first_name CHAR(20) NOT NULL,
+  surname CHAR(20) NOT NULL,
+  email VARCHAR(40) UNIQUE NOT NULL,
+  password VARCHAR(20) NOT NULL,
+  created_at CURRENT_TIMESTAMP(2)
+);
 
--- Change password of user
-UPDATE users SET password = new_password
-  WHERE users.id = 2
+INSERT INTO users (first_name, surname, email, password) VALUES
+  ('Gareth', 'Holvey', 'goat@gmail.com', 'password123');
+
+-- LESSONS TABLE -- 
+CREATE TABLE lessons (
+  -- lesson identifiers
+  id SERIAL PRIMARY KEY,
+  year_group SMALLINT NOT NULL, -- E.g. 3, 4, 5 or 6
+  category CHAR(20) NOT NULL, -- Number, Measurement, Geometry, Statistics 
+  subcategory CHAR(20) NOT NULL, -- Number -> Addition and subtraction
+
+  -- Tracking characteristics
+  created_at CURRENT_TIMESTAMP(2)
+);
+
+-- QUESTIONS TABLE --
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY,
+  category CHAR,
+  type CHAR(20)
+  FOREIGN KEY category REFERENCES lessons(category)
+  FOREIGN KEY (created_by) REFERENCES educators(email) SET NULL
+);
+
+-- TOOLTIPS TABLE -- 
+CREATE TABLE tooltips (
+  id SERIAL PRIMARY KEY,
+  -- Image to be stored with tooltip will go here (Can be Null) 
+  tooltip text NOT NULL,
+  type CHAR 
+);
+
+-- Change password of user WIP
+-- UPDATE users SET password = new_password
+--   WHERE users.id = 2

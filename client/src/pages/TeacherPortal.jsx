d5611228262e85bfb9aaacbdd27d73fce7b525fa
Harvey Lopez <up2118183@myport.ac.uk>
2026-02-07 14:53:24 +0000

    simple explanation of what the teacher portal will be

---
+++ client/src/pages/TeacherPortal.jsx
@@ -0,0 +1,10 @@
+const TeacherPortal = () => {
+  return (
+    <div>
+      <h1>This is the beginning of the Teacher Portal.</h1>
+      <p>Educators will be able to add their questions and teaching material in accordance with the national curriculum</p>
+    </div>
+  );
+}
+
+export default TeacherPortal;

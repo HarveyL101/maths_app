658897608444affbdd2cb08cadaa46a3e1303a0b
Harvey Lopez <up2118183@myport.ac.uk>
2026-02-16 04:27:13 +0000

    Curriculum object, used for the purpose of having select/options being displayed based on the parent tag (Year Group -> Topic -> Subtopic)

---
+++ client/src/components/Questions/curriculumConfig.js
@@ -0,0 +1,47 @@
+export const curriculum = {
+  3: {
+    Number: [
+      'Number & Place Value',
+      'Addition & Subtraction',
+      'Multiplication & Division',
+      'Fractions',
+    ],
+    Measurement: ['Measurement'],
+    Geometry: ['Properties of Shapes'],
+    Statistics: ['Statistics'],
+  },
+  4: {
+    Number: [
+      'Number & Place Value',
+      'Addition & Subtraction',
+      'Multiplication & Division',
+      'Fractions (Including Decimals)'
+    ],
+    Measurement: ['Measurement'],
+    Geometry: ['Properties of Shapes', 'Position & Direction'],
+    Statistics: ['Statistics'],
+  },
+  5: {
+    Number: [
+      'Number & Place Value',
+      'Addition & Subtraction',
+      'Multiplication & Division',
+      'Fractions (Including Decimals & Percentages)'
+    ],
+    Measurement: ['Measurement'],
+    Geometry: ['Properties of Shapes', 'Position & Direction'],
+    Statistics: ['Statistics'],
+  },
+  6: {
+    Number: [
+      'Number & Place Value',
+      'Addition, Subtraction, Multiplication & Division',
+      'Fractions (Inclusive Decimals & Percentages)'
+    ],
+    RatioAndProportion: ['Ratio & Proportion'],
+    Algebra: ['Algebra'],
+    Measurement: ['Measurement'],
+    Geometry: ['Properties of Shapes', 'Position & Direction'],
+    Statistics: ['Statistics'],
+  }
+}

9cfeb69139db8b9ed1fde8cef3fe7d74141e9e0e
Harvey Lopez <up2118183@myport.ac.uk>
2026-02-16 04:31:03 +0000

    baseplate for the question-making functionality

--- client/src/pages/TeacherPortal.jsx
+++ client/src/pages/TeacherPortal.jsx
@@ -1,10 +1,12 @@
+import { QuestionPortal } from "../utils";
+
 const TeacherPortal = () => {
   return (
     <div>
       <h1>This is the beginning of the Teacher Portal.</h1>
       <p>Educators will be able to add their questions and teaching material in accordance with the national curriculum</p>
 
-      
+      <QuestionPortal />
     </div>
   );
 }

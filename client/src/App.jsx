6bf73356767bb1c55946c0360013b9e3d9804f4d
Harvey Lopez <up2118183@myport.ac.uk>
2026-02-16 04:25:04 +0000

    Added guards to routes for extra protection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,66 @@
 import { Routes, Route } from 'react-router-dom';
 import { Login, Register, Home, Profile, Practice, Learn, Help, TeacherPortal } from './utils/index';
+import RequireAuth from './auth/guards/RequireAuth';
+import RequireRole from './auth/guards/RequireRole';
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/help' element={<Help />} />
         <Route path='/' element={<Login />} />
-        <Route path='/learn' element={<Learn />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/home' element={<Home />}/>
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/practice' element={<Practice />} />
-        <Route path='/portal' element={<TeacherPortal />} />
+
+        {/* Routes protected by Authorisation */}
+        <Route 
+          path='/learn' 
+          element={
+            <RequireAuth>
+              <Learn />
+            </RequireAuth>
+          } 
+        />
+        <Route 
+          path='/home' 
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          } 
+        />
+        <Route 
+          path='/profile' 
+          element={
+            <RequireAuth>
+              <Profile />
+            </RequireAuth>
+          } 
+        />
+        <Route 
+          path='/practice' 
+          element={
+            <RequireAuth>
+              <Practice />
+            </RequireAuth>
+          } 
+        />
+        <Route 
+          path='/help' 
+          element={
+            <RequireAuth>
+              <Help />
+            </RequireAuth>
+          } 
+        />
+
+        {/* Routes protected by Role */}
+        <Route 
+          path='/teacher-portal' 
+          element={
+            <RequireRole role="educator">
+              <TeacherPortal />
+            </RequireRole>
+          } 
+        />
       </Routes>
     </>
   )

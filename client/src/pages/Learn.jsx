dc0b4b85737516aa2be0305c1ea8465d42da9aad
Harvey Lopez <up2118183@myport.ac.uk>
2026-03-02 21:19:55 +0000

    slight improvement to Learn

--- client/src/pages/Learn.jsx
+++ client/src/pages/Learn.jsx
@@ -7,6 +7,11 @@ const Learn = () => {
   const [selectedTopic, setSelectedTopic] = useState('');
   const [selectedSubTopic, setSelectedSubTopic] = useState('');
   const [formData, setFormData] = useState(null);
+  const [tags, setTags] = useState({
+    Year: "",
+    Topic: "",
+    SubTopic: ""
+  });
 
   const handleFormSubmit = (submittedData) => {
     console.log("Data received in parent:", submittedData);
@@ -16,29 +21,29 @@ const Learn = () => {
   // The Values within this need to be the names of their respective DB tables (WIP)
   const PRESET_OPTIONS = {
     // Algebra
-    'Algebra': Algebra,
+    'Algebra': 'Algebra',
     // Geometry
-    'Properties of Shapes': PropertiesOfShapes,
-    'Position & Direction': PositionDirection,
+    'Properties of Shapes': 'PropertiesOfShapes',
+    'Position & Direction': 'PositionDirection',
     // Measurement
-    'Measurement': Measurement,
+    'Measurement': 'Measurement',
     // Number
-    'Addition': Addition,
-    'Subtraction': Subtraction,
-    'Multiplication': Multiplication,
-    'Division': Division,
+    'Addition': 'Addition',
+    'Subtraction': 'Subtraction',
+    'Multiplication': 'Multiplication',
+    'Division': 'Division',
     // Number/Fraction
-    'Fraction Addition': FracAddition,
-    'Fraction Subtraction': FracSubtraction,
-    'Fraction Counting Up': FracCountUp,
-    'Fractions': Fractions,
-    'Fractions (Inc. Decimals)': FractionsDec,
-    'Fractions (Inc. Decimals & Percentages)': FractionsDecPerc,
+    'Fraction Addition': 'FracAddition',
+    'Fraction Subtraction': 'FracSubtraction',
+    'Fraction Counting Up': 'FracCountUp',
+    'Fractions': 'Fractions',
+    'Fractions (Inc. Decimals)': 'FractionsDec',
+    'Fractions (Inc. Decimals & Percentages)': 'FractionsDecPerc',
     // Number/NumberPlaceValue
-    'Counting up': CountUp,
-    'Number & Place Value': NumberPlaceValue,
+    'Counting up': 'CountUp',
+    'Number & Place Value': 'NumberPlaceValue',
     // Statistics
-    'Statistics': Statistics
+    'Statistics': 'Statistics'
   }
 
   const handleYearChange = (e) => {
@@ -67,7 +72,11 @@ const Learn = () => {
             onChange={handleYearChange}
           >
             <option value="">Select Year</option>
-            {/** Code here */}
+            {[3, 4, 5, 6].map((year) => (
+              <option key={year} value={year}>
+                Year {year}
+              </option>
+            ))}
           </select>
         </label>
 
@@ -79,7 +88,12 @@ const Learn = () => {
             disabled={!selectedYear}
           >
             <option value="">Select Topic</option>
-            {/** Code here */}
+            {selectedYear &&
+              Object.keys(curriculum[selectedYear]).map((topic) => (
+                <option key={topic} value={topic}>
+                  {topic}
+                </option>
+              ))}
           </select>
         </label>
 
@@ -91,7 +105,13 @@ const Learn = () => {
             disabled={!selectedTopic}
           >
             <option value="">Select Sub-Topic</option>
-            {/** Code here */}
+            {selectedYear &&
+              selectedTopic &&
+              curriculum[selectedYear][selectedTopic].map((subTopic) => (
+                <option key={subTopic} value={subTopic}>
+                  {subTopic}
+                </option>
+              ))}
           </select>
         </label>
       </div>

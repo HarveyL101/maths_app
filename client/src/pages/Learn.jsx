a444dc400361ea59e8e61fdefde9e524f3fdd21d
Harvey Lopez <up2118183@myport.ac.uk>
2026-03-06 18:27:36 +0000

    Moved SideBar into a reusable Accordion component

--- client/src/pages/Learn.jsx
+++ client/src/pages/Learn.jsx
@@ -1,5 +1,56 @@
 import TitleBar from '../components/TitleBar.jsx';
 import SearchBar from '../components/SearchBar.jsx';
+import SideBar from '../components/SideBar.jsx';
+import Accordion from '../components/Accordion.jsx';
+
+const curriculum = [
+  {
+    title: "Year 3",
+    children: [
+      { title: "Number", content: "" },
+      { title: "Fractions", content: "" },
+      { title: "Number & Place Value", content: "" },
+      { title: "Measurement", content: "" },
+      { title: "Geometry", content: "" },
+      { title: "Statistics", content: "" }
+    ],
+  },
+  {
+    title: "Year 4",
+    children: [
+      { title: "Number", content: "" },
+      { title: "Fractions", content: "" },
+      { title: "Number & Place Value", content: "" },
+      { title: "Measurement", content: "" },
+      { title: "Geometry", content: "" },
+      { title: "Statistics", content: "" }
+    ],
+  },
+  {
+    title: "Year 5",
+    children: [
+      { title: "Number", content: "" },
+      { title: "Fractions", content: "" },
+      { title: "Number & Place Value", content: "" },
+      { title: "Measurement", content: "" },
+      { title: "Geometry", content: "" },
+      { title: "Statistics", content: "" }
+    ],
+  },
+  {
+    title: "Year 6",
+    children: [
+      { title: "Number", content: "" },
+      { title: "Fractions", content: "" },
+      { title: "Number & Place Value", content: "" },
+      { title: "Ratio & Proportion", content: "" },
+      { title: "Algebra", content: "" },
+      { title: "Measurement", content: "" },
+      { title: "Geometry", content: "" },
+      { title: "Statistics", content: "" }
+    ],
+  }
+];
 
 const Learn = () => {
   const handleFormSubmit = (submittedData) => {
@@ -7,58 +58,28 @@ const Learn = () => {
     setFormData(submittedData);
   }
 
-  // The Values within this need to be the names of their respective DB subtopics (WIP)
-  const PRESET_OPTIONS = {
-    // Algebra
-    'Algebra': Algebra,
-    // Geometry
-    'Properties of Shapes': PropertiesOfShapes,
-    'Position & Direction': PositionDirection,
-    // Measurement
-    'Measurement': Measurement,
-    // Number
-    'Addition': Addition,
-    'Subtraction': Subtraction,
-    'Multiplication': Multiplication,
-    'Division': Division,
-    // Number/Fraction
-    'Fraction Addition': FracAddition,
-    'Fraction Subtraction': FracSubtraction,
-    'Fraction Counting Up': FracCountUp,
-    'Fractions': Fractions,
-    'Fractions (Inc. Decimals)': FractionsDec,
-    'Fractions (Inc. Decimals & Percentages)': FractionsDecPerc,
-    // Number/NumberPlaceValue
-    'Counting up': CountUp,
-    'Number & Place Value': NumberPlaceValue,
-    // Statistics
-    'Statistics': Statistics
-  }
-
-  const handleYearChange = (e) => {
-    setSelectedYear(e.target.value);
-    setSelectedTopic('');
-    setSelectedSubTopic('');
-  }
-
-  const handleTopicChange = (e) => {
-    setSelectedTopic(e.target.value);
-    setSelectedSubTopic('');
-  }
-
-
 
   return (
-    <div>
+    <div className='flex flex-col min-h-screen'>
       <TitleBar />
-      <h1>This is the current page for /learn</h1>
+      
+      <div className='flex flex-1 pt-16'>
+        <div className='flex flex-col w-64 bg-white border-r border-gray-200 h-screen sticky top-16 overflow-y-auto p-4'>
+          <h2 className='text-2xl font-bold mb-4'><u>Topic Catalogue</u></h2>
+          <Accordion data={curriculum} />
+        </div>
 
-      <div> {/** This will hold the search bar portion */}
-        <SearchBar />
-      </div>
+        <div className='flex-1 p-4'>
+          <h1 className='text-2xl font-bold mb-4'>This is the current page for /Learn</h1>
 
-      <div className='portal-body'>
-        {/** Here will be the search results portion of the page */}
+          <div> {/** This will hold the search bar portion */}
+            <SearchBar />
+          </div>
+
+          <div className='portal-body'>
+            {/** Here will be the search results portion of the page */}
+          </div>
+        </div>
       </div>
     </div>
   );

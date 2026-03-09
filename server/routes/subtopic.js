31ad89d59684f934f001493ac9f97319d4311731
Harvey <UP2118183@myport.ac.uk>
2026-03-09 17:00:41 +0000

    Added functionality for searching by subtopic

---
+++ server/routes/subtopic.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../db");
+
+router.get("/", async (req, res) => {
+    const client = await pool.connect();
+    try {
+        const { year, topic } = req.query;
+        // Fetches all subtopics by year and topic
+        const subtopics = await getSubtopics(client, year, topic);
+        res.json({ subtopics });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    } finally {
+        client.release();
+    }
+});
+
+// -- WIP --
+const getSubtopics = async (client, year, topic) => {
+    const result = await client.query(`
+        SELECT 
+            st.id AS subtopic_id,
+            st.name as subtopic_name
+        FROM questions q
+        JOIN subtopic st ON q.subtopic_id = st.id
+        JOIN topic t ON st.topic_id = t.id
+        WHERE t.year_group = $1
+            AND t.name = $2
+        ORDER BY st.name ASC;
+    `, [year, topic]);
+
+    return result.rows[0]; // Array of chosen subtopics
+};

300f0c74f610d279e1228cd04bb779630046f923
Harvey <UP2118183@myport.ac.uk>
2026-03-09 17:27:55 +0000

    Added base form for maths logic to be applied to

---
+++ client/src/components/Questions/BaseQuestionForm.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect, useEffectEvent } from "react";
+import { BlockMath } from "react-katex";
+
+const BaseQuestionForm = ({ title, tooltipText, fields, createKatex, onSubmit }) => {
+    const [previewTitle, setPreviewTitle] = useState("");
+    const [params, setParams] = useState({});
+    const [previewBody, setPreviewBody] = useState(""); 
+
+    const handleChange = (name, value) => {
+        setParams(prev => ({
+            ...prev, [name]: value
+        }));
+    };
+
+    useEffect(() => {
+        setPreviewBody(createKatex(params));
+    }, [params]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        onSubmit({
+            previewTitle,
+            params
+        });
+    };
+
+    return (
+    <div className="q-container">
+
+      <div className="qform-container">
+
+        <h1>{title}</h1>
+
+        <form onSubmit={handleSubmit}>
+
+          <input
+            className="qform-input"
+            type="text"
+            value={previewTitle}
+            onChange={(e) => setPreviewTitle(e.target.value)}
+            placeholder="Question Title..."
+          />
+
+          {fields.map(field => (
+            <input
+              key={field.name}
+              className="qform-input"
+              type="text"
+              placeholder={field.placeholder}
+              value={params[field.name] || ""}
+              onChange={(e) => handleChange(field.name, e.target.value)}
+            />
+          ))}
+
+          <button type="submit">Submit</button>
+
+        </form>
+
+      </div>
+
+      <div className="preview-container">
+
+        <h1>{previewTitle || "Question Title Here"}</h1>
+
+        {previewBody ? (
+          <BlockMath math={previewBody} />
+        ) : (
+          <p>A complete calculation will appear here.</p>
+        )}
+
+      </div>
+
+    </div>
+    );
+};
+
+export default BaseQuestionForm;

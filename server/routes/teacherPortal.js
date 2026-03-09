e0274580cd747620e737daa4b90ff3afd9dd6f21
Harvey <UP2118183@myport.ac.uk>
2026-03-09 17:02:11 +0000

    Minor tweak

--- server/routes/teacherPortal.js
+++ server/routes/teacherPortal.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 module.exports = (authenticateJWT, hasRole) => {
   router.get("/", authenticateJWT, hasRole("educator"), (req, res) => {
-    res.json({ message: "Welcome to the Teacher Portal!" });
+    res.json({ message: "Welcome Educator!" });
   });
 
   return router;

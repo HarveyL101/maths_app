37c1022c558b8c34061f4dbe490e76c092eaccbb
Harvey Lopez <up2118183@myport.ac.uk>
2026-04-24 00:25:23 +0100

    Updated with values for the upcoming colour scheme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,26 @@
 export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,}"],
   theme: {
     extend: {
-      colors: { // Preset colours for uniform app layout
-        primary: '',
-        secondary: '',
-        accent: '', 
-      },
-      spacing: { // Work in progress
-        'WIP': '18rem',
+      colors: {
+        primary: '#53096E',
+        secondary: '#9912CB',
+        accent: '#1A919F',
+
+        bg: {
+          light: '#EDEDED',
+          dark: '#1A1A1A',
+        },
+
+        text: {
+          light: '#111111',
+          dark: '#FFFFFF',
+        },
+
+        surface: {
+          light: '#FFFFFF',
+          dark: '#2A2A2A',
+        }
       },
     }
   }
